bfb06a0b94dbfb7ba6deb2344939a6a06b51d8fb
plally <11771225+plally@users.noreply.github.com>
2020-11-11 23:33:35 -0500

    kill tmux session if server doesnt successfully restart

--- functions/cfc3-restart/lambda_function.py
+++ functions/cfc3-restart/lambda_function.py
@@ -4,9 +4,9 @@ from sshrunner import run_command
 
 def lambda_handler(event, context):
     return run_command(
-        command="cd /var/steam/gmod/cfc3/ && timeout 20 ./cfc3 restart",
-        host="172.245.14.234", 
-        port=22, 
+        command="cd /var/steam/gmod/cfc3/ && timeout 20 ./cfc3 restart || (tmux kill-session -t cfc3 && ./cfc3 start)",
+        host="172.245.14.234",
+        port=22,
         username="steam",
         private_key="-----BEGIN RSA PRIVATE KEY-----\n" + os.environ["private_key"] + "\n-----END RSA PRIVATE KEY-----"
     )
