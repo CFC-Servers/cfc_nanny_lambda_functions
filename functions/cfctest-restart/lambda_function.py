613808b0a67a3f13d50c76b2a59b8c26a434cd6a
plally <11771225+plally@users.noreply.github.com>
2020-11-14 19:02:59 -0500

    change bash command

--- functions/cfctest-restart/lambda_function.py
+++ functions/cfctest-restart/lambda_function.py
@@ -4,7 +4,7 @@ from sshrunner import run_command
 
 def lambda_handler(event, context):
     return run_command(
-        command="cd /var/steam/gmod/cfc-test/ && timeout 20 ./cfctest restart || (tmux kill-session -t cfc3 && ./cfctest start)",
+        command="cd /var/steam/gmod/cfc-test/; timeout 30 ./cfctest restart || (tmux kill-session -t cfc3 && ./cfctest start)",
         host="172.245.14.234",
         port=22,
         username="steam",
