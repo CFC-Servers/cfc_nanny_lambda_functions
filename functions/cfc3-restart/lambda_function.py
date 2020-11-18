42aaae1a2531be009893ed70f9f04bc3e4343eee
plally <11771225+plally@users.noreply.github.com>
2020-11-17 23:08:55 -0500

    fix restart command killing own process, set check=False on ssh runner

--- functions/cfc3-restart/lambda_function.py
+++ functions/cfc3-restart/lambda_function.py
@@ -5,11 +5,14 @@ import re
 name = "cfc3"
 directory = "/var/steam/gmod/cfc3/"
 timeout_seconds = 30
+
+name = f"[{name[0]}]" + name[1:] # prevents command from killing itsself
+
 command = f'cd {directory}; ' \
         f'timeout {timeout_seconds} ./{name} restart || ' \
-        f'(echo "{name} could not restart within {timeout_seconds} killing process" && ' \
-        f'pkill -fe "{name}.cfg" && ' \
-        f'./{name} start)'
+        f'( printf "\n{name} could not restart within {timeout_seconds} killing process" && ' \
+        f'pkill -fe "{name}.cfg"; ' \
+        f'./{name} start )'
 
 def clean_output(out):
     lines = out.split("\n")
@@ -20,10 +23,11 @@ def clean_output(out):
         line = re.sub(r"(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]", "", line) # remove ANSI formatting codes
         line = re.sub(r"/var/[^\s]*", "****", line)
         cleaned_lines.append( line )
-
+ 
     return "\n".join(cleaned_lines)
 
 def lambda_handler(event, context):
+    print("Running: ", command)
     output = run_command(
         command=command,
         host="172.245.14.234",
@@ -32,8 +36,10 @@ def lambda_handler(event, context):
         private_key="-----BEGIN RSA PRIVATE KEY-----\n" + os.environ["private_key"] + "\n-----END RSA PRIVATE KEY-----"
     )
 
-    print("Command outputed: \n", output)
+    print("Command outputed: \n", output.stdout)
 
-    output["stdout"] = clean_output(output["stdout"])
+    output.stdout = clean_output(output.stdout)
 
-    return output
+    return {
+        "stdout": output.stdout
+    }
