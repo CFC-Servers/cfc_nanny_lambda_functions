444fa18fc40d6a7e27ae27b9b322b81b0ee81d12
Brandon Sturgeon <brandon@brandonsturgeon.com>
2020-11-18 01:22:52 -0800

    Add multi-auth option to auth function

--- functions/cfc-nanny-auth/lambda_function.py
+++ functions/cfc-nanny-auth/lambda_function.py
@@ -1,13 +1,6 @@
 import json
 import os
 
-def get_expected_auth(route):
-    route = route.replace("/", "")
-    route = route.replace("-", "_")
-    route = route.upper()
-    
-    return os.environ.get("{}_AUTH".format(route), None)
-
 def is_authorized(authorized):
     return { "isAuthorized": authorized }
 
@@ -17,31 +10,52 @@ def deny():
 def accept():
     return is_authorized(True)
 
+def get_allowed_keys(route):
+    route = route.replace("/", "")
+    route = route.replace("-", "_")
+    route = route.upper()
+
+    allowed_key_names = os.environ.get("{}_AUTH".format(route), None)
+
+    if not allowed_key_names:
+        return None
+
+    allowed_keys = set()
+    allowed_key_names = auth_keys.split(",")
+    for key_name in allowed_key_names:
+        key = os.environ.get(key_name, None)
+
+        if not key:
+            print("Erorr! Couldn't find key in environment: {}".format(key_name))
+            continue
+
+        allowed_keys.add(key)
+
+    return allowed_keys
+
 def lambda_handler(event, context):
-    auth = event.get("headers", {}).get("authorization", None)
-    
-    if auth is None:
+    key = event.get("headers", {}).get("authorization", None)
+
+    if key is None:
         print("Auth is none, returning False")
         return deny()
-        
+
     route = event.get("rawPath", None)
-    
+
     if route is None:
         print("Route is none, returning False")
         return deny()
 
-    expected = get_expected_auth(route)
-    
-    if expected is None:
-        print("Route not present in AUTH, returning False")
-        return deny()
-        
-    print("{} == {} = {}".format(auth, expected, auth == expected))
+    allowed_keys = get_allowed_keys(route)
 
-    is_authorized = auth == expected
-    
+    if allowed_keys is None:
+        print("Couldn't find any allowed keys for given route, '{}'. Denying.".format(route))
+        return deny()
+
+    is_authorized = key in allowed_keys
+
     if is_authorized:
         return accept()
-    
+
     return deny()
 
