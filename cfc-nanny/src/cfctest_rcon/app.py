dc64f1e13b4139b50ee3068398f70c2af33e5d1c
Brandon Sturgeon <brandon@brandonsturgeon.com>
2020-11-24 13:54:36 -0800

    Convert to new SAM structure

---
+++ cfc-nanny/src/cfctest_rcon/app.py
@@ -0,0 +1,15 @@
+from cfcrconinterface import RCONInterface
+
+def lambda_handler(event, context):
+    interface = RCONInterface()
+
+    command = event.get("body", None)
+    if command is None:
+        print("Command is none, returning empty response")
+        return {}
+
+    success, response = interface.issue_command(command)
+    if success:
+        return {"response": response}
+
+    return {"error": str(response)}
