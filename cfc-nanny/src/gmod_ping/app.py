be0f61db7a79aa436574faca03e8eb1beea03960
Brandon Sturgeon <brandon@brandonsturgeon.com>
2020-11-25 18:47:15 -0800

    Add cache

--- cfc-nanny/src/gmod_ping/app.py
+++ cfc-nanny/src/gmod_ping/app.py
@@ -1,5 +1,9 @@
 from cfcrconinterface import RCONInterface
 import json
+import time
+
+CACHE_DURATION = 5
+STATUS_CACHE = {}
 
 def response(status=200, response=None, error=None):
     body = {}
@@ -23,11 +27,44 @@ def up():
 def down():
     return response(response="server-is-down")
 
+def get_cached(destination):
+    response = STATUS_CACHE.get(destination, None)
+
+    if response is None:
+        return None
+
+    now = time.time()
+    expiration = response.get("expires", 0)
+    is_stale = now >= expiration
+
+    if is_stale:
+        return None
+
+    return response.get("response", {})
+
+def set_cached(destination, response):
+    now = time.time()
+
+    struct = {
+        "response": response,
+        "expires": now + CACHE_DURATION
+    }
+
+    STATUS_CACHE[destination] = struct
+
 def lambda_handler(event, context):
     interface = RCONInterface()
 
-    success, result = interface.issue_command("status")
-    if success:
-        return up()
+    destination = f"{interface.address}:{interface.port}"
 
-    return down()
+    cached_response = get_cached(destination)
+    if cached_response:
+        return cached_response
+
+    success = interface.issue_command("status")
+
+    response = up() if success else down()
+
+    set_cached(destination, response)
+
+    return response
